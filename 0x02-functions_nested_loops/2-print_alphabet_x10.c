9356d61dbe32cec27ddff6ba7782387f6fce61d2
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-06-21 16:17:51 +0300

    PcTest

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * description Print_alphabet 10x - function that will print the alphabet 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+    int line;
+
+    for (line = 0; line <=9; line++)
+    {
+        print_alphabet();
+    }
+}
+
+
