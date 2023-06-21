4a8a84049c7b7595aaebba18c31e66affe52e155
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-06-21 16:28:40 +0300

    PcTest

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,16 @@
 
 void print_alphabet_x10(void)
 {
-    int line;
+    int line, ch;
 
     for (line = 0; line <=9; line++)
     {
-        print_alphabet();
-    }
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+           _putchar(ch);
+        }
+        _putchar('\n');
+        }
 }
 
 
