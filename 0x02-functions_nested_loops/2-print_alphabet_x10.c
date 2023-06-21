e0b104cc528284f0f140fa4f24574ee3b83bfdc8
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-06-21 16:43:36 +0300

    PcTest

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * description Print_alphabet 10x - function that will print the alphabet 10 times
+ * description Print_alphabet 10x - function that will print the alphabet
+ * 10 times
 */
 
 void print_alphabet_x10(void)
 {
     int line, ch;
-
-    for (line = 0; line <=9; line++)
+    for (line = 0; line <= 9; line++)
     {
         for (ch = 'a'; ch <= 'z'; ch++)
         {
            _putchar(ch);
         }
         _putchar('\n');
-        }
+    }
 }
 
 
