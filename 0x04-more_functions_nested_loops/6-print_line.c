c5bdb25279a6c9de63595ea2fed52de088562b88
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-06-22 19:27:43 +0300

    FromPc

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * more_numbers - a function that prints the numbers
- * from 0 to 9
- *
- * Return: 0-9, excluding 2 and 4, followed by new line
+ * print_line - print line
+ *@n: is input how '_' do you need to print
+ * Return: _ n numbers
  */
 void print_line(int n)
 {
