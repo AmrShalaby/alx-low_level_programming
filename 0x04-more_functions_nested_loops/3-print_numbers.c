5fc82d845b9ad64612c1cab523e7ed9a21a6d297
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-06-22 18:24:02 +0300

    FromPc

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_numbers - function that print 0..9
+*
+*Return: returns 0
+*/
+void print_numbers(void)
+{
+for(int i = 0; i <=9; i++)
+    printf("%d", i);
+
+printf("\n");
+return (0);
+}
+
+
