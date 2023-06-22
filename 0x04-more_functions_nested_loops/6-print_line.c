9a497516bdddb58a234708e109a6a59367c29bb1
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-06-22 19:26:05 +0300

    FromPc

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - a function that prints the numbers
+ * from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+
+
+
+
