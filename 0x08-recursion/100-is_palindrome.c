1de2d3de53f1771c007baa47e680a7c4c54f949f
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-07-06 00:13:43 +0300

    FromPC

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,13 +11,13 @@ int is_palindrome_helper(char *s, int len, int i)
 {
 if (i >= len / 2)
 {
-return 1;
+return(1);
 }
 if (s[i] != s[len - i - 1])
 {
-return 0;
+return(0);
 }
-return is_palindrome_helper(s, len, i + 1);
+return is_palindrome_helper(s, len, (i + 1));
 }
 /**
  * is_palindrome - Checks if a string is a palindrome
