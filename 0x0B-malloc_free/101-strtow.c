f8959c728a3499d7968f75cdec6614b05c0da3e6
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-07-11 02:05:00 +0300

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,44 +10,42 @@
 char **strtow(char *str)
 {
 char **words;
-int i, j, k, len, count;
+int i, j, k, count, len;
 
 if (str == NULL || *str == '\0')
 return (NULL);
 
-len = 0;
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-len++;
-}
-
-words = malloc(sizeof(char *) * (len + 1));
+count = count_words(str);
+words = malloc(sizeof(char *) * (count + 1));
 if (words == NULL)
 return (NULL);
 
-count = 0;
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0, j = 0; j < count; j++)
 {
-if (str[i] != ' ')
+while (str[i] == ' ')
+i++;
+
+len = 0;
+while (str[i + len] != ' ' && str[i + len] != '\0')
+len++;
+
+words[j] = malloc(sizeof(char) * (len + 1));
+if (words[j] == NULL)
 {
-for (j = i; str[j] != ' ' && str[j] != '\0'; j++)
-;
-words[count] = malloc(sizeof(char) * (j - i + 1));
-if (words[count] == NULL)
-{
-for (k = 0; k < count; k++)
-free(words[k]);
+for (k = 0; k < j; k++)
+    free(words[k]);
 free(words);
 return (NULL);
 }
-for (k = 0; i < j; k++, i++)
-words[count][k] = str[i];
-words[count][k] = '\0';
-count++;
-}
-}
-words[count] = NULL;
 
+for (k = 0; k < len; k++)
+words[j][k] = str[i + k];
+words[j][k] = '\0';
+
+i += len;
+}
+
+words[count] = NULL;
 return (words);
 }
+
