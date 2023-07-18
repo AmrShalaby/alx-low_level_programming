6f99c375305a64d3e1a5a9858ca05c855eab7062
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-07-18 04:16:29 +0300

    for pc

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,9 +11,10 @@
  */
 void init_dog(dog_t *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
