1c3127c7411bcb830e0829f8bafad977306881a8
AmrShalaby <AmrShalabyOfficial@gmail.com>
2023-07-18 04:53:20 +0300

    for pc

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Prints the name of the file it was compiled from.
+ *
+ * @return 0 on success.
+ */
+int main(void) {
+  char *file_name = __FILE__;
+
+  /* Betty style: Indent each level by 4 spaces. */
+  printf("The name of the file is: %s\n", file_name);
+
+  return 0;
+}
