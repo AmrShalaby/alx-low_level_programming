1117bbad51c21ac9015c1518736bd8d40ba46913
AmrShalaby <30528860+AmrShalaby@users.noreply.github.com>
2023-08-05 01:04:10 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef _OBJECT__LIKE_MACRO_H
-#define _OBJECT__LIKE_MACRO_H
-
-#define SIZE 1024
-
-#endif /* _OBJECT__LIKE_MACRO_H */
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SUM_H
-#define _Sum_h
-
-#define SUM(x, y) ((x) + (y))
-
-#endif /* _SUM_H */
